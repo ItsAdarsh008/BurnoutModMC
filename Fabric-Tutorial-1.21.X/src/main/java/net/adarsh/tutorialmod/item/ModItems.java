d68a3a4b225fea54f965cfd16017b3527843cb36
Adarsh <adarshthoduvakkal@gmail.com>
2025-07-16 14:27:53 -0400

    registered pink garnet in INGREDIENTS, adding texture

--- Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
+++ Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
@@ -1,7 +1,9 @@
 package net.adarsh.tutorialmod.item;
 
 import net.adarsh.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -17,5 +19,6 @@ public class ModItems {
     public static void registerModItems() {
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
 
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> entries.add(PINK_GARNET));
     }
 }
