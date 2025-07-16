1c073456c338ecccf6eac77fb6d40029b434f66b
Adarsh <adarshthoduvakkal@gmail.com>
2025-07-16 11:30:39 -0400

    added new dependencies, Registers

--- Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/TutorialMod.java
+++ Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.adarsh.tutorialmod;
 
+import net.adarsh.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 	}
 } //backup
