1d5b4d375cbbc25a1cb83eeae6785786befd3241
Adarsh <adarshthoduvakkal@gmail.com>
2025-07-16 14:24:47 -0400

    added 'pink garnet', sample item to add, to be replaced later

--- Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
+++ Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
 
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
