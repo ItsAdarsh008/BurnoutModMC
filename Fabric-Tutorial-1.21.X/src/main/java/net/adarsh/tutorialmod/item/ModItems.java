1b0d8096dfa2db8a7260765310b1b357d5c9b8c4
Adarsh <adarshthoduvakkal@gmail.com>
2025-07-16 21:22:48 -0400

    ladies and gentlemen, the first disc works!!

--- Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
+++ Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.adarsh.tutorialmod.item;
 
 import net.adarsh.tutorialmod.TutorialMod;
+import net.adarsh.tutorialmod.sound.ModSounds;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,6 +13,11 @@ public class ModItems {
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+//Discs begin
+
+    public static final Item KEY_MUSIC_DISC = registerItem("key_music_disc",
+            new Item(new Item.Settings().jukeboxPlayable(ModSounds.KEY_KEY).maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
--- Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/sound/ModSounds.java
+++ Fabric-Tutorial-1.21.X/src/main/java/net/adarsh/tutorialmod/sound/ModSounds.java
@@ -1,8 +1,11 @@
 package net.adarsh.tutorialmod.sound;
 
 import net.adarsh.tutorialmod.TutorialMod;
+import net.minecraft.block.jukebox.JukeboxSong;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
@@ -16,6 +19,10 @@ public class ModSounds {
 
     public static final BlockSoundGroup GARNET_BLOCK_SOUNDS = new BlockSoundGroup(1f, 1f,
             GARNET_BLOCK_BREAK,GARNET_BLOCK_STEP,GARNET_BLOCK_PLACE,GARNET_BLOCK_HIT,GARNET_BLOCK_FALL);
+
+    public static final SoundEvent KEY = registerSoundEvent("key");
+    public static final RegistryKey<JukeboxSong> KEY_KEY =
+            RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(TutorialMod.MOD_ID, "key"));
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = Identifier.of(TutorialMod.MOD_ID, name);
